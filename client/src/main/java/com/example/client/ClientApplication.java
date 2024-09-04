770a76d6f4f6f74db12c49c73445bd27949eb424
Josh Long <josh@joshlong.com>
2024-09-04 03:06:28 -0700

    cleaned up the need for the success handler. do everything in the GrantedAuthoritiesMapper`

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -2,17 +2,17 @@ package com.example.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.simple.JdbcClient;
-import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.stereotype.Component;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -36,17 +36,40 @@ public class ClientApplication {
     }
 
     @Bean
-    GrantedAuthoritiesMapper userAuthoritiesMapper(JdbcUserDetailsManager userDetailsManager) {
+    GrantedAuthoritiesMapper userAuthoritiesMapper(JdbcClient jdbcClient, JdbcUserDetailsManager jdbcUserDetailsManager) {
         return (authorities) -> {
             var mappedAuthorities = new HashSet<GrantedAuthority>();
 
             authorities.forEach(authority -> {
                 if (authority instanceof OidcUserAuthority oidcUserAuthority) {
+
+
                     var userInfo = oidcUserAuthority.getUserInfo();
                     var username = userInfo.getSubject();
-                    var userDetails = userDetailsManager.loadUserByUsername(username);
-                    mappedAuthorities.addAll(userDetails.getAuthorities());
-                } else if (authority instanceof OAuth2UserAuthority oauth2UserAuthority) {
+
+                    jdbcClient.sql("""
+                                        insert into users (username, password, enabled) values (?,?,?)
+                                        on conflict do nothing ;
+                                    """)
+                            .params(username, UUID.randomUUID(), true)
+                            .update();
+
+                    jdbcClient.sql("""
+                                        insert into authorities  (username, authority) values (?,?)
+                                        on conflict do nothing ;
+                                    """)
+                            .params(username, "USER")
+                            .update();
+
+                    // 
+                    var userDetails = jdbcUserDetailsManager.loadUserByUsername(username);
+                    var roles = userDetails.getAuthorities()
+                            .stream()
+                            .map(ga -> new SimpleGrantedAuthority("ROLE_" + ga.getAuthority()))
+                            .toList();
+                    mappedAuthorities.addAll(roles);
+                } // 
+                else if (authority instanceof OAuth2UserAuthority oauth2UserAuthority) {
 
                     //todo
                     // var userAttributes = oauth2UserAuthority.getAttributes();
@@ -58,37 +81,16 @@ public class ClientApplication {
             return mappedAuthorities;
         };
     }
-}
 
-@Component
-class AuthenticationListener implements ApplicationListener<AuthenticationSuccessEvent> {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-    private final JdbcClient jdbcClient;
-
-    AuthenticationListener(JdbcClient jdbcClient) {
-        this.jdbcClient = jdbcClient;
-    }
-
-    @Override
-    public void onApplicationEvent(AuthenticationSuccessEvent event) {
-        var username = event.getAuthentication().getName();
-        var password = UUID.randomUUID().toString();
-        var enabled = true;
-        this.jdbcClient.sql("""
-                            insert into users (username, password, enabled) values (?,?,?)
-                            on conflict do nothing ;
-                        """)
-                .params(username, password, enabled)
-                .update();
-
-        this.jdbcClient.sql("""
-                            insert into authorities  (username, authority) values (?,?)
-                            on conflict do nothing ;
-                        """)
-                .params(username, "USER")
-                .update();
-
-
+        return http
+                .authorizeHttpRequests(h -> h
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
     }
 }
 
@@ -104,11 +106,7 @@ class SecuredController {
 
     @GetMapping("/")
     Map<String, String> hello(Principal principal) {
-        var user = SecurityContextHolder
-                .getContextHolderStrategy()
-                .getContext()
-                .getAuthentication();
-        System.out.println("authentication [" + user.getName() + ":" + user.getAuthorities() + "]");
+        System.out.println(principal.getClass().getName());
         return Map.of("message", "hello, " + principal.getName());
     }
 }
